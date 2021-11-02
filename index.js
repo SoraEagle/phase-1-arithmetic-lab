812b0f9a0f8612d70e6086f87d59a7373fb4b536
Matthew <phillips.matthew829@gmail.com>
2021-11-02 15:39:49 -0500

    commit tree 8761a2bd7e39c97086f9b4dc4fc725890cde649c

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+//multiply
+    let num1 = 2;
+    let num2 = 31;
+    let multiply = num1 * num2;
+
+    //random
+    let random = Math.ceil((Math.random() * 10) + 1);
+
+    //mod
+    let num3 = 9;
+    let num4 = 5;
+    let mod = num3 % num4;
+
+    //max
+    let max = Math.max(2, 3, 5, 7, 11, 13, 17, 19, 20);
